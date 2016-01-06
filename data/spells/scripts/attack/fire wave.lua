2ca2f11ff6eeaaa0fc59968fdc19016494f0ed8d
Bruno Minervino <brunominervino1993@gmail.com>
2016-01-06 21:04:10 -0200

    Correção de Spells e descrição nas Runas

--- data/spells/scripts/attack/fire wave.lua
+++ data/spells/scripts/attack/fire wave.lua
@@ -1,9 +1,7 @@
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HITBYFIRE)
-
-local area = createCombatArea(AREA_WAVE4, AREADIAGONAL_WAVE4)
-combat:setArea(area)
+combat:setArea(createCombatArea(AREA_WAVE4, AREADIAGONAL_WAVE4))
 
 function onGetFormulaValues(player, level, maglevel)
 	local min = (level / 5) + (maglevel * 1.2) + 7
@@ -13,6 +11,6 @@ end
 
 combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
-function onCastSpell(creature, var)
-	return combat:execute(creature, var)
+function onCastSpell(creature, variant)
+	return combat:execute(creature, variant)
 end
